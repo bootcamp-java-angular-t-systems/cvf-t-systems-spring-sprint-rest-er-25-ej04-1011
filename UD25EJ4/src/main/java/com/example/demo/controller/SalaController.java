30bfe65b81f3d94c82300299c3a8e1f236ca2d13
carlavelasco15 <carlavelasco15@gmail.com>
2023-11-11 00:50:32 +0100

    change @Controller to @RestController

--- UD25EJ4/src/main/java/com/example/demo/controller/SalaController.java
+++ UD25EJ4/src/main/java/com/example/demo/controller/SalaController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.Sala;
 import com.example.demo.service.SalaServiceImpl;
 
-@Controller
+@RestController
 @RequestMapping("/sala")
 public class SalaController {
 
