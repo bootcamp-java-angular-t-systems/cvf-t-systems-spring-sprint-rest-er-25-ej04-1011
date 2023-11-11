b8f3d9a414921fc6a56f2b39be9e5edfe2a62fc8
carlavelasco15 <carlavelasco15@gmail.com>
2023-11-11 01:02:43 +0100

    add 5 inserts per table

---
+++ UD25EJ4/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- Inserts para la tabla "peliculas"
+INSERT INTO peliculas (nombre) VALUES ('Pelicula1');
+INSERT INTO peliculas (nombre) VALUES ('Pelicula2');
+INSERT INTO peliculas (nombre) VALUES ('Pelicula3');
+INSERT INTO peliculas (nombre) VALUES ('Pelicula4');
+INSERT INTO peliculas (nombre) VALUES ('Pelicula5');
+
+-- Inserts para la tabla "salas"
+INSERT INTO salas (codigo, nombre, calificacion_edad, pelicula_id) VALUES (1, 'Sala1', 12, 1);
+INSERT INTO salas (codigo, nombre, calificacion_edad, pelicula_id) VALUES (2, 'Sala2', 18, 2);
+INSERT INTO salas (codigo, nombre, calificacion_edad, pelicula_id) VALUES (3, 'Sala3', 12, 3);
+INSERT INTO salas (codigo, nombre, calificacion_edad, pelicula_id) VALUES (4, 'Sala4', 18, 4);
+INSERT INTO salas (codigo, nombre, calificacion_edad, pelicula_id) VALUES (5, 'Sala5', 12, 5);
